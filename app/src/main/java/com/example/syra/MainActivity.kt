6349eee19709296324112613505ff47777abd40c
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2024-11-29 10:25:36 +0100

    feat: implement call mqtt manager

--- app/src/main/java/com/example/syra/MainActivity.kt
+++ app/src/main/java/com/example/syra/MainActivity.kt
@@ -1,11 +1,79 @@
 package com.example.syra
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import com.example.syra.service.MqttManager
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
+import org.eclipse.paho.client.mqttv3.MqttCallback
+import org.eclipse.paho.client.mqttv3.MqttMessage
 
 class MainActivity : ComponentActivity() {
+    private lateinit var mqttManager: MqttManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mqttManager = MqttManager(
+            context = this,
+            brokerUrl = "",
+            username = "",
+            password = ""
+        )
+
+        mqttManager.connect (
+            callback = object : MqttCallback {
+                override fun connectionLost(cause: Throwable?) {
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, "Connection lost!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun messageArrived(topic: String?, message: MqttMessage?) {
+                    //Handle arriving messages
+                }
+
+                override fun deliveryComplete(token: IMqttDeliveryToken?) {
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, "Message sent!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            },
+            onSuccess = {
+                Toast.makeText(this, "Connected to the broker", Toast.LENGTH_SHORT).show()
+            },
+            onError = { e ->
+                Toast.makeText(this, "Error: ${e.message}", Toast.LENGTH_LONG).show()
+            }
+        )
+
+        val btnMqtt = findViewById<Button>(R.id.btnMQTT)
+
+        btnMqtt.setOnClickListener{
+            mqttManager.publishMessage(
+                topic = "my/test/topic",
+                message = "Hello from Kotlin!",
+                onSuccess = {
+                    Toast.makeText(this, "Message published!", Toast.LENGTH_SHORT).show()
+                },
+                onError = { e ->
+                    Toast.makeText(this, "Error: ${e.message}", Toast.LENGTH_LONG).show()
+                }
+            )
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mqttManager.disconnect(
+            onSuccess = {
+                Toast.makeText(this, "Disconnected from the broker!", Toast.LENGTH_SHORT).show()
+            },
+            onError = { e ->
+                Toast.makeText(this, "Disconnection error: ${e.message}", Toast.LENGTH_LONG).show()
+            }
+        )
     }
 }
