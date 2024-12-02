79848b2cb7e5919e6cea5c4982e99c683f149a1b
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2024-12-02 11:14:38 +0100

    feat: create card adapter

---
+++ app/src/main/java/com/example/syra/adapter/CardAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.syra.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.example.syra.R
+
+class CardAdapter (
+    private val context: Context,
+    private val devicesNames : List<String>)
+    : BaseAdapter() {
+
+    override fun getCount(): Int {
+        return devicesNames.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return Any()
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View = convertView ?: LayoutInflater.from(context).inflate(R.layout.smart_device_card, parent, false)
+
+        val name: TextView = view.findViewById(R.id.smartDeviceName)
+
+        name.text = devicesNames[position]
+
+        return view
+    }
+}
